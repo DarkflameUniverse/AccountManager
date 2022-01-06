463b9f0ca488dc08541a78f4f05d507146686c31
Aaron Kimbre <aronwk.aaron@gmail.com>
2022-01-06 12:58:57 -0600

    no overwrite and quite unzip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # unzip brickdb from client to the right places
-unzip /app/luclient/res/brickdb.zip -d app/luclient/res/
+unzip -n -q /app/luclient/res/brickdb.zip -d app/luclient/res/
 
 flask db upgrade
 gunicorn -b :8000 -w 4 wsgi:app
